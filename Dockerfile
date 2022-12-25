4ee33684054d019fe19ed98103bd780eb324302a
ehrenb <branden.a.ehrenreich@gmail.com>
2022-12-25 00:52:38 -0500

    apt had an old version of binwalk, so got latest working in Dockerfile by using a documented patch

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,24 @@ RUN rm /tmp/requirements.txt
 
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt update &&\
-    apt install -y binwalk
+# install Binwalk
+
+# dep.sh requires 'python' to exist, not just 'python3'
+RUN ln -s $(which python3) /usr/bin/python
+
+# a patched version of binwalk's deps.sh
+# that applies this fix: https://github.com/devttys0/sasquatch/issues/48#issuecomment-1267506233
+COPY deps.sh /tmp/
+RUN chmod +x /tmp/deps.sh
+RUN wget https://github.com/ReFirmLabs/binwalk/archive/refs/tags/v2.3.3.zip -P /machina &&\
+    cd /machina && unzip v2.3.3.zip
+
+# execute patched deps.sh
+RUN cd /tmp && ./deps.sh --yes
+
+# install binwalk python api
+RUN cd /machina/binwalk-2.3.3 &&\
+    python3 setup.py install
 
 COPY BinwalkAnalysis.json /schemas/
 
--- src/binwalkanalysis.py
+++ src/binwalkanalysis.py
@@ -1,5 +1,8 @@
 import base64
+import glob
 import json
+from pathlib import Path
+import shutil
 
 from machina.core.worker import Worker
 
@@ -9,30 +12,69 @@ class BinwalkAnalysis(Worker):
     types = [
         'jffs2'
     ]
+    next_queues = ['Identifier']
 
     def __init__(self, *args, **kwargs):
         super(BinwalkAnalysis, self).__init__(*args, **kwargs)
 
     def callback(self, data, properties):
-        # self.logger.info(data)
         data = json.loads(data)
 
         # resolve path
         target = self.get_binary_path(data['ts'], data['hashes']['md5'])
         self.logger.info(f"resolved path: {target}")
 
-        # with gzip.open(target, 'rb') as f:
-        #     data_encoded = base64.b64encode(f.read()).decode()
-        #     body = {
-        #             "data": data_encoded,
-        #             "origin": {
-        #                 "ts": data['ts'],
-        #                 "md5": data['hashes']['md5'],
-        #                 "id": data['id'], #I think this is the only field needed, we can grab the unique node based on id alone
-        #                 "type": data['type']}
-        #             }
+        for module in binwalk.scan(
+            target,
+            '--run-as=root',
+            '--matryoshka',
+            '--depth=8',
+            signature=True,
+            quiet=True,
+            extract=True):
 
-        # channel = self.get_channel(self.config['rabbitmq'])
-        # channel.basic_publish(exchange='machina',
-        #     routing_key='Identifier',
-        #     body=json.dumps(body))
+            for result in module.results:
+                if result.file.path in module.extractor.output:
+                    # These are files that binwalk carved out of the original firmware image, a la dd
+                    # if result.offset in module.extractor.output[result.file.path].carved:
+                    #     print(f"Carved data from offset 0x%X to %s" % (result.offset, module.extractor.output[result.file.path].carved[result.offset])
+                    # These are files/directories created by extraction utilities (gunzip, tar, unsquashfs, etc)
+                    if result.offset in module.extractor.output[result.file.path].extracted:
+                        # print "Extracted %d files from offset 0x%X to '%s' using '%s'" % (len(module.extractor.output[result.file.path].extracted[result.offset].files),
+                        #                                                                 result.offset,
+                        #                                                                 module.extractor.output[result.file.path].extracted[result.offset].files[0],
+                        #                                                                 module.extractor.output[result.file.path].extracted[result.offset].command)
+                        files = module.extractor.output[result.file.path].extracted[result.offset].files
+
+                        flattened_extracted_files = []
+                        for f in files:
+
+                            # if directory was extracted, recursively collect all files 
+                            if Path(f).is_dir():
+                                _files = [i for i in glob.glob(f'{f}/**/*', recursive=True) if Path(i).is_file()]
+                                flattened_extracted_files.extend(_files)
+
+                            # if file was extracted, just append it 
+                            if Path(f).is_file():
+                                flattened_extracted_files.append(f)
+
+                        for f in flattened_extracted_files:
+                            with open(f, 'rb') as f:
+                                fdata = f.read()
+                            
+                            self.logger.info(f"publishing {f} to Identifier")
+
+                            # Send each internal file to Identifier for analysis
+                            data_encoded = base64.b64encode(fdata).decode()
+                            body = json.dumps({
+                                    "data": data_encoded,
+                                    "origin": {
+                                        "ts": data['ts'],
+                                        "md5": data['hashes']['md5'],
+                                        "id": data['id'], #I think this is the only field needed, we can grab the unique node based on id alone
+                                        "type": data['type']}
+                                    })
+                            self.publish_next(body)
+                            
+                        # cleanup root directory
+                        shutil.rmtree(files[0])
